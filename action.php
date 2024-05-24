03b648276cd2f225b865db334e24e54ca6b5e347
mmmuri <mark.muriithi@strathmore.edu>
2024-05-24 16:21:15 +0300

    link buttons to signup to action page

--- action.php
+++ action.php
@@ -1,7 +1,24 @@
-
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Thank You</title>
+</head>
+<body>
+<?php
 echo 'Thanks for signing up to our HIE program';
-echo '<br>';
-echo "<h2>Thank you for contacting us, $name!</h2>";
-print "We will get back to you at $email soon.</p>";
+echo '<br><br>';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+
+    echo "<h2>Thank you for contacting us, $name!</h2>";
+    echo "<p>We will get back to you at $email soon.</p>";
+} else {
+    echo "Invalid request method.";
+}
 ?>
+</body>
+</html>
